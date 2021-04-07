68d82f910edb5f7e2ecd3d02b6002a227f8da3a1
Emerson Duarte <raasjava@gmail.com>
2021-04-07 14:09:57 -0300

    feat: metodo post da api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const router = require('./routes/routes');
 
+const router = require('./routes/routes');
 const app = express();
+
 app.use(express.json());
 app.use(router);
 
--- src/database/knexfile.js
+++ src/database/knexfile.js
@@ -1,14 +1,13 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
-
   development: {
     client: 'postgresql',
     connection: {
-      host:'localhost',
-      database: 'adocao_pets',
-      user:     'usr_pg',
-      password: 'usr_pg',
+      host: process.env.DB_HOST,
+      database: process.env.DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
       migrations: {
         directory: './database/migrations',
       }
